3164b30807b118aa80881a8ef380f2e5c7a90cd5
cufa <facundo_corigliano@hotmail.com>
2021-10-16 16:43:41 -0300

    prueba

--- PBII20212QParcial1Grupo11/src/ar/edu/unlam/pb2/Pelicula.java
+++ PBII20212QParcial1Grupo11/src/ar/edu/unlam/pb2/Pelicula.java
@@ -4,12 +4,46 @@ public class Pelicula {
 	Integer id;
 	String nombre;
 	TipoDePelicula tipo;
-	Escena escenas[] = new Escena[];
+	Escena escenas[] = new Escena[5];
 	
-	public Pelicula(Integer id, String nombre, TipoDePelicula tipo, Double duracion) {
+	public Pelicula(Integer id, String nombre, TipoDePelicula tipo) {
 		super();
 		this.id = id;
 		this.nombre = nombre;
 		this.tipo = tipo;
 	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public TipoDePelicula getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(TipoDePelicula tipo) {
+		this.tipo = tipo;
+	}
+
+	public Escena[] getEscenas() {
+		return escenas;
+	}
+
+	public void setEscenas(Escena[] escenas) {
+		this.escenas = escenas;
+	}
+	
+	
 }
