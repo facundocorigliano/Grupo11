27814b45dc85a0a57febd285a815d0d88d90fdda
cufa <facundo_corigliano@hotmail.com>
2021-10-16 22:54:19 -0300

    Limpieza de código

--- PBII20212QParcial1Grupo11/testPelicula/ar/edu/unlam/pb2/PeliculasTest.java
+++ PBII20212QParcial1Grupo11/testPelicula/ar/edu/unlam/pb2/PeliculasTest.java
@@ -6,30 +6,6 @@ import org.junit.Test;
 
 
 public class PeliculasTest {
-
-	@Test
-    public void queSePuedaCrearUnActorDeComedia() {
-        Pelicula grownUp = new Pelicula(111, "Grown Up", TipoDePelicula.COMEDIA);
-        assertEquals(111, grownUp.getId(), 0);
-    }
-
-    @Test
-    public void queSePuedaCrearUnActorDeAccion() {
-    	Pelicula rambo = new Pelicula(222, "Rambo", TipoDePelicula.ACCION);
-        assertEquals(222, rambo.getId(), 0);
-    }
-
-    @Test
-    public void queSePuedaCrearUnActorDeRomance() {
-        Pelicula sombrasDeGrey = new Pelicula(333, "50 Sombras de Grey", TipoDePelicula.ROMANTICA);
-        assertEquals(333, sombrasDeGrey.getId(), 0);
-    }
-
-    @Test
-    public void queSePuedaCrearUnActorDeComediaRomantica() {
-    	Pelicula amigosConDerechos = new Pelicula(444, "Amigos con Derechos", TipoDePelicula.COMEDIAROMANTICA);
-        assertEquals(444, amigosConDerechos.getId(), 0);
-    }
     
     @Test
     public void queSePuedaCrearUnaEscena() {
@@ -37,13 +13,6 @@ public class PeliculasTest {
     	assertTrue(escena1.getEsExterior());
     	
     }
-    
-
-   /* @Test
-    public void queSePuedaAgregarUnElenco() {
-    	Actor jimCarrey = new ActorComedia(1, "Jim Carrey", TipoDeHumor.FAMILIAR);
-    	Elenco protagonistas = new Elenco(null);
-    }*/
     
     
 }
